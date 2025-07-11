54fda003c0a9c0d2515be436c50014f17718d0c3
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-07-11 17:24:37 +0800

    login logout functions

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import { generateToken } from "../utils/generateToken.js";
-import { passwordEncrypt } from "../utils/passwordBcrypt.js";
+import { passwordCompare, passwordEncrypt } from "../utils/passwordBcrypt.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -44,9 +44,36 @@ export const signup = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
-export const login = (req, res) => {
-  res.send("login route");
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const isPasswordCorrect = await passwordCompare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Login Failed :", error);
+    res.status(500).json({ message: "Internal server Error" });
+  }
 };
 export const logout = (req, res) => {
-  res.send("logout route");
+  try {
+    res.clearCookie("jwt");
+    res.status(200).json({ message: "Logout Successfully" });
+  } catch (error) {
+    console.log("error in logout: ", error);
+    res.status(500).json({ message: "Internal server Error" });
+  }
 };
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 
+// you should use "post" for login so it will use request body so the password and other important information in the database will not be fetched since if you use "get" it will fetch all the data
 router.post("/login", login);
 
 router.post("/logout", logout);
