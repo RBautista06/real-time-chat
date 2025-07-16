5362d61b9aa28acb14a151e2cbbbe15ed7eeaf20
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-07-17 00:31:01 +0800

    fix ui scrolling behaivior

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -72,7 +72,7 @@ const ProfilePage = () => {
   }
 
   return (
-    <div className="min-h-screen bg-background">
+    <div className="h-auto bg-background">
       <div className="container mx-auto px-4 py-8">
         <div className="max-w-2xl mx-auto">
           {/* Header */}
