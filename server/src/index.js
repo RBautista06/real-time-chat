b0237c43030622d03fdda68cda20422e3f46f60c
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-07-16 22:28:11 +0800

    stable local

--- server/src/index.js
+++ server/src/index.js
@@ -1,39 +1,29 @@
 import express from "express";
-import http from "http";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import path from "path";
-
-import { connectionDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { setupSocket } from "./lib/socket.js";
-
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectionDB } from "./lib/db.js";
+import cors from "cors";
+import { app, io, server } from "./lib/socket.js"; // import app from the socket so it will be real time
+import path from "path";
 dotenv.config();
-
-const app = express();
-const server = http.createServer(app);
-setupSocket(server); // Sets up socket.io and attaches to HTTP server
 
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
-// Middleware
-app.use(express.json());
+app.use(express.json()); // converts the request body to json
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: "http://localhost:5173", // ✅ Only allow requests from this origin (your frontend)
+    credentials: true, // ✅ Allow cookies (like JWT) to be sent with the request
   })
 );
 
-// Routes
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes); // routes for signup, login, logout
 app.use("/api/messages", messageRoutes);
 
-// Serve static files in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
@@ -42,8 +32,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Start server
 server.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+  console.log(`server running in Port: ${PORT}`);
   connectionDB();
 });
